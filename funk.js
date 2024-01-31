2880b324964b1028c82b84331b103db36e69f06b
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-01-31 15:20:36 +0100

    def public

---
+++ funk.js
@@ -0,0 +1,13 @@
+class Polo{
+    constructor(id, nev, ar, kep, leiras){
+        this.id = id;
+        this.nev = nev;
+        this.ar=ar;
+        this.kep = kep;
+        this.leiras = leiras;
+    }
+}
+
+const list = element.Polo;
+
+list.add(Polo(1, "Retro Gamer férfi póló", 6900, "kepek/polo1"))
